9c8d2fbbdf3aa94f0aef6b2e8322b59b9a692f9c
Radek Novotny <novotnyradekcz@gmail.com>
2026-08-25 20:16:57 -0300

    Added agentic loop

--- main.py
+++ main.py
@@ -8,50 +8,66 @@ from openai import OpenAI
 from call_function import available_functions, call_function
 from prompts import system_prompt
 
-parser = argparse.ArgumentParser(description="Chatbot")
-_ = parser.add_argument("user_prompt", type=str, help="User prompt")
-_ = parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-args = parser.parse_args()
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Chatbot")
+    _ = parser.add_argument("user_prompt", type=str, help="User prompt")
+    _ = parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-_ = load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
+    _ = load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
 
-if api_key is None:
-    raise RuntimeError("`OPENROUTER_API_KEY` not set. Add it to your .env file.")
+    if api_key is None:
+        raise RuntimeError("`OPENROUTER_API_KEY` not set. Add it to your .env file.")
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key,
-)
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
 
-messages = [
-    {"role": "system", "content": system_prompt},
-    {"role": "user", "content": args.user_prompt},
-]
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": args.user_prompt},
+    ]
 
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=messages,
-    tools=available_functions,
-)
 
-if response.usage is None:
-    raise RuntimeError("`response.usage` is None. API request likely failed.")
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage.prompt_tokens}")
-    print(f"Response tokens: {response.usage.completion_tokens}")
-if response.choices[0].message.tool_calls is not None:
-    for tool_call in response.choices[0].message.tool_calls:
-        function_args = json.loads(tool_call.function.arguments or "{}")
-        print(f"Calling function: {tool_call.function.name}({function_args})")
+    for _ in range(20):
+        response = client.chat.completions.create(
+            model="openrouter/free",
+            messages=messages,
+            tools=available_functions,
+        )
 
-        result_message = call_function(tool_call)
-
-        if result_message["content"] is None or result_message["content"] == "":
-            raise Excpetion("no tool content returned")
-
+        if response.usage is None:
+            raise RuntimeError("`response.usage` is None. API request likely failed.")
         if args.verbose:
-            print(f"-> {result_message['content']}")
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage.prompt_tokens}")
+            print(f"Response tokens: {response.usage.completion_tokens}")
 
-print(f"Response:\n{response.choices[0].message.content}")
+        message = response.choices[0].message
+        messages.append(message)
+
+        if response.choices[0].message.tool_calls is not None:
+            for tool_call in response.choices[0].message.tool_calls:
+                function_args = json.loads(tool_call.function.arguments or "{}")
+                print(f"Calling function: {tool_call.function.name}({function_args})")
+
+                result_message = call_function(tool_call)
+
+                if result_message["content"] is None or result_message["content"] == "":
+                    raise Excpetion("no tool content returned")
+
+                if args.verbose:
+                    print(f"-> {result_message['content']}")
+
+                messages.append(result_message)
+        else:
+            print(f"Response:\n{response.choices[0].message.content}")
+            return
+
+    print("No solution reached within 20 iterations.")
+    exit(1)
+
+if __name__ == "__main__":
+    main()
