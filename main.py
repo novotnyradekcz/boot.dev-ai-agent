ee09ebe3e07da5f24ed164ea3f168e07a07947fb
Radek Novotny <novotnyradekcz@gmail.com>
2026-08-24 21:55:05 -0300

    Adding function calls

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import argparse
+import json
 import os
 
 from dotenv import load_dotenv
 from openai import OpenAI
 
+from call_function import available_functions
 from prompts import system_prompt
 
 parser = argparse.ArgumentParser(description="Chatbot")
@@ -30,6 +32,7 @@ messages = [
 response = client.chat.completions.create(
     model="openrouter/free",
     messages=messages,
+    tools=available_functions,
 )
 
 if response.usage is None:
@@ -38,4 +41,8 @@ if args.verbose:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage.prompt_tokens}")
     print(f"Response tokens: {response.usage.completion_tokens}")
+if response.choices[0].message.tool_calls is not None:
+    for tool_call in response.choices[0].message.tool_calls:
+        function_args = json.loads(tool_call.function.arguments or "{}")
+        print(f"Calling function: {tool_call.function.name}({function_args})")
 print(f"Response:\n{response.choices[0].message.content}")
