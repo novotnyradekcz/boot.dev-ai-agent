1931ad6a800c86d417a918d616d0fe0dd88932c2
Radek Novotny <novotnyradekcz@gmail.com>
2026-08-24 22:57:39 -0300

    Actually calling functions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-from call_function import available_functions
+from call_function import available_functions, call_function
 from prompts import system_prompt
 
 parser = argparse.ArgumentParser(description="Chatbot")
@@ -45,4 +45,13 @@ if response.choices[0].message.tool_calls is not None:
     for tool_call in response.choices[0].message.tool_calls:
         function_args = json.loads(tool_call.function.arguments or "{}")
         print(f"Calling function: {tool_call.function.name}({function_args})")
+
+        result_message = call_function(tool_call)
+
+        if result_message["content"] is None or result_message["content"] == "":
+            raise Excpetion("no tool content returned")
+
+        if args.verbose:
+            print(f"-> {result_message['content']}")
+
 print(f"Response:\n{response.choices[0].message.content}")
